9efa794d2d5acc79a626d6d544638e52031e9af0
Mårten Wikström <5657237+mwikstrom@users.noreply.github.com>
2022-04-12 14:42:25 +0200

    tracked vars + refresh in error response

--- src/ErrorResponse.ts
+++ src/ErrorResponse.ts
@@ -1,4 +1,6 @@
+import { TrackedVariable } from "./EvaluateScriptResponse";
 import { GenericResponse, isGenericResponse } from "./GenericResponse";
+import { ScriptValue } from "./ScriptValue";
 
 /**
  * The response that is sent when an error occurred
@@ -6,7 +8,23 @@ import { GenericResponse, isGenericResponse } from "./GenericResponse";
  */
 export interface ErrorResponse extends GenericResponse<"error">{
     message: string;
-}
+
+    /**
+     * Optionally includes the global variables that were accessed during script evaluation.
+     * 
+     * This property is omitted unless the error occurred during script evaluation and may 
+     * be omitted unless the script is evaluated with the `track` option.
+     */
+    vars?: Map<string, TrackedVariable>;
+
+    /**
+     * The value assigned to `this.refresh` during script evaluation.
+     * 
+     * This property is omitted unless the error occurred during script evaluation and may
+     * be omitted unless the script assigned `this.refresh`.
+     */
+    refresh?: ScriptValue;
+ }
 
 /**
  * Determines whether something is an {@link ErrorResponse}
